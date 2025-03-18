63c311e28e3c95d87d6434bb9ff8526784f011a7
Cassidy Motto <ST10067751@rcconnect.edu.za>
2025-03-18 14:02:50 +0200

    - the controller allows CRUD (Create, Read, Update, Delete) operations

---
+++ EventEase/Controllers/VenueController.cs
@@ -0,0 +1,71 @@
+﻿using EventEase.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EventEase.Controllers
+{
+    public class VenueController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public VenueController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Venues.ToListAsync());
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Venue venue)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(venue);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(venue);
+        }
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null) return NotFound();
+
+            var venue = await _context.Venues.FindAsync(id);
+            if (venue == null) return NotFound();
+
+            return View(venue);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, Venue venue)
+        {
+            if (id != venue.VenueId) return NotFound();
+
+            if (ModelState.IsValid)
+            {
+                _context.Update(venue);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(venue);
+        }
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            var venue = await _context.Venues.FindAsync(id);
+            if (venue == null) return NotFound();
+
+            _context.Venues.Remove(venue);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
