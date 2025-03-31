242625fbe8aecb69dbd007df23fcc04552a1a7a9
Cassidy Motto <ST10067751@rcconnect.edu.za>
2025-03-31 17:07:17 +0200

    - The new ApplicationDbContext that is migrated to Azure Database

---
+++ EventEase/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using EventEase.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace EventEase.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<IdentityUser>(options)
+    {
+        public DbSet<Venue> Venues { get; set; }
+        public DbSet<Event> Events { get; set; }
+        public DbSet<Booking> Bookings { get; set; }
+    }
+}
