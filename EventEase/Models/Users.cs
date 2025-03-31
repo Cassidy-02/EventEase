95b3b74566053bf4cd767f0dbc4ce150cd3330d4
Cassidy Motto <ST10067751@rcconnect.edu.za>
2025-03-31 17:06:09 +0200

    - Changed entities

---
+++ EventEase/Models/Users.cs
@@ -0,0 +1,10 @@
+﻿namespace EventEase.Models
+{
+    public class Users
+    {
+        public int UserId { get; set; } //Primary Key
+        public required string Username { get; set; }
+        public required string PasswordHash { get; set; }
+        public List<string> Roles { get; set; } = new List<string>();
+    }
+}
