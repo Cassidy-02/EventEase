4938e6ec35b7d7be86ab77a1773479595cec3a61
Cassidy Motto <ST10067751@rcconnect.edu.za>
2025-04-02 18:11:46 +0200

    - Migrated to Azure and updated database

---
+++ EventEase/Migrations/20250402143024_InitialCreate.cs
@@ -0,0 +1,196 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EventEase.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Booking_Event_EventId",
+                table: "Booking");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Bookings_Venues_VenueId",
+                table: "Booking");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Event_Venue_VenueId",
+                table: "Event");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Venues",
+                table: "Venues");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Event",
+                table: "Event");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Booking",
+                table: "Booking");
+
+            migrationBuilder.RenameTable(
+                name: "Venue",
+                newName: "Venue");
+
+            migrationBuilder.RenameTable(
+                name: "Event",
+                newName: "Event");
+
+            migrationBuilder.RenameTable(
+                name: "Booking",
+                newName: "Booking");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Event_VenueId",
+                table: "Event",
+                newName: "IX_Event_VenueId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Booking_VenueId",
+                table: "Booking",
+                newName: "IX_Booking_VenueId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Booking_EventId",
+                table: "Booking",
+                newName: "IX_Booking_EventId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Venue",
+                table: "Venue",
+                column: "VenueId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Event",
+                table: "Event",
+                column: "EventId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Booking",
+                table: "Booking",
+                column: "BookingId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Booking_Event_EventId",
+                table: "Booking",
+                column: "EventId",
+                principalTable: "Event",
+                principalColumn: "EventId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Booking_Venue_VenueId",
+                table: "Booking",
+                column: "VenueId",
+                principalTable: "Venue",
+                principalColumn: "VenueId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Event_Venue_VenueId",
+                table: "Event",
+                column: "VenueId",
+                principalTable: "Venue",
+                principalColumn: "VenueId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Booking_Event_EventId",
+                table: "Booking");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Booking_Venue_VenueId",
+                table: "Booking");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Event_Venue_VenueId",
+                table: "Event");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Venue",
+                table: "Venue");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Event",
+                table: "Event");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Booking",
+                table: "Booking");
+
+            migrationBuilder.RenameTable(
+                name: "Venue",
+                newName: "Venues");
+
+            migrationBuilder.RenameTable(
+                name: "Event",
+                newName: "Event");
+
+            migrationBuilder.RenameTable(
+                name: "Booking",
+                newName: "Bookings");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Event_VenueId",
+                table: "Events",
+                newName: "IX_Events_VenueId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Booking_VenueId",
+                table: "Bookings",
+                newName: "IX_Bookings_VenueId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Booking_EventId",
+                table: "Bookings",
+                newName: "IX_Bookings_EventId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Venues",
+                table: "Venues",
+                column: "VenueId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Events",
+                table: "Events",
+                column: "EventId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Bookings",
+                table: "Bookings",
+                column: "BookingId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Bookings_Events_EventId",
+                table: "Bookings",
+                column: "EventId",
+                principalTable: "Events",
+                principalColumn: "EventId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Bookings_Venues_VenueId",
+                table: "Bookings",
+                column: "VenueId",
+                principalTable: "Venues",
+                principalColumn: "VenueId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Events_Venues_VenueId",
+                table: "Events",
+                column: "VenueId",
+                principalTable: "Venues",
+                principalColumn: "VenueId");
+        }
+    }
+}
